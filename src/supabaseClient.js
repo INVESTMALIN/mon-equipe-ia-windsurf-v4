94101ed5098930de778c0a7c4a7a58aa68f526d5
Julinhio <julinhio@hotmail.com>
2025-05-27 10:16:34 +0200

    Nouveau déploiement après config Vercel anon key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://gbdturqxlxyvdtezjwxa.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdiZHR1cnF4bHh5dmR0ZXpqd3hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4NzgzMjMsImV4cCI6MjA2MzQ1NDMyM30.JbO7l0jJwF2-9AuCzf8vhApgFky8L2culhOmlItiUoM'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
